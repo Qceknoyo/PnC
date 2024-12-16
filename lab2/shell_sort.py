ce8e37049ae09eabe05fbf7e20b52fa075d0bcb3
sokol <sokol@DESKTOP-P8BOCFF>
2024-12-17 01:36:02 +0300

    Добавьте файлы проекта.

---
+++ lab2/shell_sort.py
@@ -0,0 +1,32 @@
+﻿# Функция сортировки методом Шелла
+def shell_sort(arr, reverse=False):
+    # Устанавливаем начальный интервал (gap)
+    gap = len(arr) // 2
+
+    # Основной цикл: продолжаем, пока gap не станет 0
+    while gap > 0:
+        # Проходим по массиву от gap до конца
+        for value in range(gap, len(arr)):
+            # Сохраняем текущий элемент и его индекс
+            current_value = arr[value]
+            pos = value
+
+            # Если выбран reverse (по убыванию)
+            if reverse:
+                # Переставляем элементы, пока предыдущий меньше текущего
+                while pos >= gap and arr[pos - gap] < current_value:
+                    arr[pos] = arr[pos - gap]
+                    pos -= gap
+            else:
+                # Иначе переставляем элементы, пока предыдущий больше текущего
+                while pos >= gap and arr[pos - gap] > current_value:
+                    arr[pos] = arr[pos - gap]
+                    pos -= gap
+
+            # Вставляем текущий элемент на его новое место
+            arr[pos] = current_value
+
+        # Уменьшаем gap для следующей итерации
+        gap //= 2
+
+    return arr
---
+++ lab2/tests.py
@@ -0,0 +1,28 @@
+﻿from shell_sort import shell_sort
+from data_loader import random_arr
+
+def test_shell_sort():
+    print("Введите длину массива")
+   
+   # Создаем случайный массив указанной длины
+    arr = random_arr(int(input()))
+    
+    user_input = int(input("Сортировать по возрастанию? (1 - да, 0 - нет): "))
+    if user_input == 1:
+        reverse = False  # Сортировка по возрастанию
+    else:
+        reverse = True  # Сортировка по убыванию
+
+   
+    # Выводим созданный тестовый массив
+    print("Тестовый массив:", arr)
+    
+   # Сортируем массив методом Шелла 
+    shell_sort(arr,reverse=reverse)
+   # Выводим отсортированный  
+    print("Отсортированный массив:", arr)
+
+
+# Запускаем тестовую функцию, если этот файл выполняется как основная программа
+if __name__ == "__main__":
+    test_shell_sort()
